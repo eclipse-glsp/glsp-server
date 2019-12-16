39658e0b8239b4a016219c8ef27fad5518ab7f23
Tobias Ortmayr <tortmayr@eclipsesource.com>
2019-12-16 10:21:31 +0100

    Update JDK preferences and fix wrong copyright headers

--- plugins/org.eclipse.glsp.server.websocket/src/org/eclipse/glsp/server/websocket/GLSPConfigurator.java
+++ plugins/org.eclipse.glsp.server.websocket/src/org/eclipse/glsp/server/websocket/GLSPConfigurator.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * Copyright (c) 2019 EclipseSource and others.
- *  
- *   This program and the accompanying materials are made available under the
- *   terms of the Eclipse Public License v. 2.0 which is available at
- *   https://www.eclipse.org/legal/epl-2.0.
- *  
- *   This Source Code may also be made available under the following Secondary
- *   Licenses when the conditions for such availability set forth in the Eclipse
- *   Public License v. 2.0 are satisfied: GNU General Public License, version 2
- *   with the GNU Classpath Exception which is available at
- *   https://www.gnu.org/software/classpath/license.html.
- *  
- *   SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ******************************************************************************/
 package org.eclipse.glsp.server.websocket;
 
@@ -24,48 +24,50 @@ import javax.websocket.server.HandshakeRequest;
 import javax.websocket.server.ServerEndpointConfig;
 import javax.websocket.server.ServerEndpointConfig.Configurator;
 
-import org.eclipse.jetty.websocket.jsr356.server.ContainerDefaultConfigurator;
-
 import com.google.inject.Injector;
 
 public class GLSPConfigurator extends Configurator {
-	private Configurator containerConfigurator;
-	private Injector injector;
+   private Configurator containerConfigurator;
+   private final Injector injector;
 
-	public GLSPConfigurator(Injector injector) {
-		this.injector = injector;
-	}
+   public GLSPConfigurator(final Injector injector) {
+      this.injector = injector;
+   }
 
-	Configurator getContainerConfigurator() {
-		if (containerConfigurator == null) {
-			containerConfigurator = ServiceLoader.load(javax.websocket.server.ServerEndpointConfig.Configurator.class)
-					.findFirst().orElse(new ContainerDefaultConfigurator());
-		}
-		return containerConfigurator;
-	}
+   Configurator getContainerConfigurator() {
+      if (containerConfigurator == null) {
+         ServiceLoader<Configurator> loader = ServiceLoader
+            .load(javax.websocket.server.ServerEndpointConfig.Configurator.class);
+         if (loader.iterator().hasNext()) {
+            containerConfigurator = loader.iterator().next();
+         }
+      }
+      return containerConfigurator;
+   }
 
-	@Override
-	public String getNegotiatedSubprotocol(List<String> supported, List<String> requested) {
-		return this.getContainerConfigurator().getNegotiatedSubprotocol(supported, requested);
-	}
+   @Override
+   public String getNegotiatedSubprotocol(final List<String> supported, final List<String> requested) {
+      return this.getContainerConfigurator().getNegotiatedSubprotocol(supported, requested);
+   }
 
-	@Override
-	public List<Extension> getNegotiatedExtensions(List<Extension> installed, List<Extension> requested) {
-		return this.getContainerConfigurator().getNegotiatedExtensions(installed, requested);
-	}
+   @Override
+   public List<Extension> getNegotiatedExtensions(final List<Extension> installed, final List<Extension> requested) {
+      return this.getContainerConfigurator().getNegotiatedExtensions(installed, requested);
+   }
 
-	@Override
-	public boolean checkOrigin(String originHeaderValue) {
-		return this.getContainerConfigurator().checkOrigin(originHeaderValue);
-	}
+   @Override
+   public boolean checkOrigin(final String originHeaderValue) {
+      return this.getContainerConfigurator().checkOrigin(originHeaderValue);
+   }
 
-	@Override
-	public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
-		return injector.getInstance(endpointClass);
-	}
+   @Override
+   public <T> T getEndpointInstance(final Class<T> endpointClass) throws InstantiationException {
+      return injector.getInstance(endpointClass);
+   }
 
-	@Override
-	public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
-		/* do nothing */
-	}
+   @Override
+   public void modifyHandshake(final ServerEndpointConfig sec, final HandshakeRequest request,
+      final HandshakeResponse response) {
+      /* do nothing */
+   }
 }
--- plugins/org.eclipse.glsp.server.websocket/src/org/eclipse/glsp/server/websocket/GLSPServerEndpoint.java
+++ plugins/org.eclipse.glsp.server.websocket/src/org/eclipse/glsp/server/websocket/GLSPServerEndpoint.java
@@ -1,50 +1,50 @@
 /*******************************************************************************
  * Copyright (c) 2019 EclipseSource and others.
- *  
- *   This program and the accompanying materials are made available under the
- *   terms of the Eclipse Public License v. 2.0 which is available at
- *   https://www.eclipse.org/legal/epl-2.0.
- *  
- *   This Source Code may also be made available under the following Secondary
- *   Licenses when the conditions for such availability set forth in the Eclipse
- *   Public License v. 2.0 are satisfied: GNU General Public License, version 2
- *   with the GNU Classpath Exception which is available at
- *   https://www.gnu.org/software/classpath/license.html.
- *  
- *   SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ******************************************************************************/
 package org.eclipse.glsp.server.websocket;
 
 import java.util.Collection;
 
-import org.eclipse.lsp4j.jsonrpc.Launcher.Builder;
-import org.eclipse.lsp4j.websocket.WebSocketEndpoint;
-
 import org.eclipse.glsp.api.json.GsonConfigurator;
 import org.eclipse.glsp.api.jsonrpc.GLSPClient;
 import org.eclipse.glsp.api.jsonrpc.GLSPClientAware;
 import org.eclipse.glsp.api.jsonrpc.GLSPServer;
+import org.eclipse.lsp4j.jsonrpc.Launcher.Builder;
+import org.eclipse.lsp4j.websocket.WebSocketEndpoint;
+
 import com.google.inject.Inject;
 
 public class GLSPServerEndpoint extends WebSocketEndpoint<GLSPClient> {
 
-	@Inject
-	private GLSPServer glspServer;
+   @Inject
+   private GLSPServer glspServer;
 
-	@Inject
-	private GsonConfigurator gsonConfigurator;
+   @Inject
+   private GsonConfigurator gsonConfigurator;
 
-	@Override
-	protected void configure(Builder<GLSPClient> builder) {
-		builder.setLocalService(glspServer);
-		builder.setRemoteInterface(GLSPClient.class);
-		builder.configureGson(gsonConfigurator::configureGsonBuilder);
-	}
+   @Override
+   protected void configure(final Builder<GLSPClient> builder) {
+      builder.setLocalService(glspServer);
+      builder.setRemoteInterface(GLSPClient.class);
+      builder.configureGson(gsonConfigurator::configureGsonBuilder);
+   }
 
-	@Override
-	protected void connect(Collection<Object> localServices, GLSPClient remoteProxy) {
-		localServices.stream().filter(GLSPClientAware.class::isInstance).map(GLSPClientAware.class::cast)
-				.forEach(ca -> ca.connect(remoteProxy));
-	}
+   @Override
+   protected void connect(final Collection<Object> localServices, final GLSPClient remoteProxy) {
+      localServices.stream().filter(GLSPClientAware.class::isInstance).map(GLSPClientAware.class::cast)
+         .forEach(ca -> ca.connect(remoteProxy));
+   }
 
 }

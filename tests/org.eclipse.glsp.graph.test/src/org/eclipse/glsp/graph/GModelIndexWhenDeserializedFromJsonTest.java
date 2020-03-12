873fb0d7645398c64bb17ed148d21869272460f2
Philip Langer <planger@eclipsesource.com>
2020-03-12 15:15:56 +0100

    Fix test case again after verifying that test failures are reported

--- tests/org.eclipse.glsp.graph.test/src/org/eclipse/glsp/graph/GModelIndexWhenDeserializedFromJsonTest.java
+++ tests/org.eclipse.glsp.graph.test/src/org/eclipse/glsp/graph/GModelIndexWhenDeserializedFromJsonTest.java
@@ -50,7 +50,6 @@ public class GModelIndexWhenDeserializedFromJsonTest {
       assertEquals(graph.getChildren().get(0), index.get("node1").get());
       assertEquals(graph.getChildren().get(1), index.get("node2").get());
       assertEquals(graph.getChildren().get(2), index.get("edge12").get());
-      assertEquals(true, false);
    }
 
    @Test

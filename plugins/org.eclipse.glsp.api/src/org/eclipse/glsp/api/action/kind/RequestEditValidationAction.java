b7651fb9aaa1a2db686a083976bcae27a25823ff
Philip Langer <planger@eclipsesource.com>
2020-04-01 12:04:39 +0200

    Add generic edit validation support for validating input while editing
    https://github.com/eclipse-glsp/glsp/issues/56

---
+++ plugins/org.eclipse.glsp.api/src/org/eclipse/glsp/api/action/kind/RequestEditValidationAction.java
@@ -0,0 +1,43 @@
+/********************************************************************************
+ * Copyright (c) 2020 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+package org.eclipse.glsp.api.action.kind;
+
+import org.eclipse.glsp.api.action.Action;
+
+public class RequestEditValidationAction extends RequestAction<SetEditValidationResultAction> {
+
+   private String modelElementId;
+   private String text;
+
+   public RequestEditValidationAction() {
+      super(Action.Kind.REQUEST_EDIT_VALIDATION);
+   }
+
+   public RequestEditValidationAction(final String kind, final String modelElementId, final String text) {
+      this();
+      this.modelElementId = modelElementId;
+      this.text = text;
+   }
+
+   public String getModelElementId() { return modelElementId; }
+
+   public void setModelElementId(final String modelElementId) { this.modelElementId = modelElementId; }
+
+   public String getText() { return text; }
+
+   public void setText(final String text) { this.text = text; }
+
+}

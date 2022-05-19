3e4c60b2f634590baa38ecc9997e5e078a2f103b
Tobias Ortmayr <tortmayr@eclipsesource.com>
2022-05-19 02:11:31 -0700

    Remove final modifier from DefaultClientSessionManager (#167)
    + Fix type in `org.eclipse.glsp.server/internal/diagram` package

--- plugins/org.eclipse.glsp.server/src/org/eclipse/glsp/server/internal/diagram/DefaultServerConfigurationContribution.java
+++ plugins/org.eclipse.glsp.server/src/org/eclipse/glsp/server/internal/diagram/DefaultServerConfigurationContribution.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2021 EclipseSource and others.
+ * Copyright (c) 2021-2022 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -13,7 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-package org.eclipse.glsp.server.internal.digram;
+package org.eclipse.glsp.server.internal.diagram;
 
 import static org.eclipse.glsp.server.di.GLSPModule.CLIENT_ACTIONS;
 
